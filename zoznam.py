bdad9dd0feade9a31d5a261583b8ffc658aa3fbf
cpython420 <71539873+cpython420@users.noreply.github.com>
2021-02-07 17:37:43 +0100

    Create zoznam.py

---
+++ zoznam.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  7 16:44:40 2021
+
+@author: adam
+"""
+
+#zoznam s nasimi cislami
+zoznam = [6,14,22,30,50,65,32,38,10,98,9,23,91,78,13]
+
+#vypise vsetky prvky zoznamu, tak ze for cyklus listuje cisla 0-14, co su indexy jednotlivych cisiel v liste
+for i in range(0, len(zoznam)):
+    print(zoznam[i])
