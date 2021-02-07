85688d18bf285105bd5f70b1d8a92c288e1c2ba8
cpython420 <71539873+cpython420@users.noreply.github.com>
2021-02-07 17:27:29 +0100

    Update radius.py

--- radius.py
+++ radius.py
@@ -1,5 +1,5 @@
 from math import pi
-
+#premenna r sa rovna vstupu od uzivatela, ktory premenime na integer, pri vyziadani vstupu uzivatela vypise argument funkcie v " "
 r = int(input("Napis polomer kruhu: "))
-
+#funkcia print vo vete vypise polomer kruhu, ktory je povodne int a musime ho premenit na string, a plochu, ktora je matematicka operacia, premenena na string
 print("Kruh s polomerom " + str(r) + " ma obsah " + str(pi*r**2))
